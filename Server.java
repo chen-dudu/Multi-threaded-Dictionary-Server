c662febe17c4124d9a8b9d78bd6d9def92588e0b
chen-dudu <liguoc@student.unimelb.edu.au>
2021-04-03 16:18:17 +0800

    finished implementing multi-threading for server, added a logic to detect illegal port number, added ownership information

--- Server.java
+++ Server.java
@@ -1,21 +1,35 @@
+/**
+ * Author: Liguo Chen
+ * Student ID: 851090
+ */
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    // number of threads in the pool
+    public static final int NUM_THREADS = 4;
+
+    public static void main(String[] args) {
 
         if (args.length < 2) {
             // not enough input params provided
-            System.out.println("Usage: java -jar DictionaryServer.jar <port> <dictionary-file>");
+            System.out.println("Usage: java -jar DictionaryServer.jar <port> <dictionary-file>\n");
             return;
         }
 
         int port = Integer.parseInt(args[0]);
         String dictFile = args[1];
+
+        if (port < 1024 || port > 65535) {
+            System.err.println("Please choose a reasonable port number and start the server again.\n");
+            return;
+        }
 
         HashMap<String, String> dict = new HashMap<>();
 
@@ -26,7 +40,7 @@ public class Server {
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(dictFile)));
         }
         catch (FileNotFoundException e) {
-            System.err.println("The provided dictionary file " + dictFile + " cannot be found");
+            System.err.println("The provided dictionary file " + dictFile + " cannot be found.\n");
             return;
         }
         String word, meaning;
@@ -37,7 +51,7 @@ public class Server {
             }
         }
         catch (IOException ioe) {
-            System.out.println("System failed to process the provided dictionary file.");
+            System.out.println("System failed to process the provided dictionary file.\n");
             return;
         }
 
@@ -47,77 +61,28 @@ public class Server {
             server = new ServerSocket(port);
         }
         catch (IOException ioe) {
-            System.err.println("System failed to open a socket for the server.");
+            System.err.println("System failed to open a socket for the server.\nYou may want to try a different port.\n");
             return;
         }
 
-        // an inf loop to keep the server online
-        Socket client = server.accept();
-        while (true) {
-            System.out.println("Server is listening at port " + port);
-//            Socket client;
-//            try {
-//                client = server.accept();
-//            }
-//            catch (IOException ioe) {
-//                System.err.println("System failed to connect to client.");
-//                continue;
-////                return;
-//            }
-            System.out.println("Connected to a client");
+        ExecutorService pool = Executors.newFixedThreadPool(NUM_THREADS);
 
-            BufferedReader in;
-            BufferedWriter out;
+        // an inf loop to keep the server online
+        while (true) {
+            System.out.println("Server is listening at port " + port + "\n");
+            Socket client;
             try {
-                in = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
-                out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
+                client = server.accept();
             }
             catch (IOException ioe) {
-                System.err.println("System failed to get stream from client socket.");
+                System.err.println("System failed to connect to client.\n");
                 continue;
+//                return;
             }
+            System.out.println("Connected to a client.\n");
 
-            try {
-                String operation = in.readLine();
-                String query = in.readLine();
-                String response = null;
-                switch (operation) {
-                    case "search":
-                        String result = dict.get(query);
-                        response = result == null ? "No match result" : result;
-                        break;
-                    case "add":
-                        if (dict.containsKey(query)) {
-                            response = "1";
-                        }
-                        else {
-                            String newMeaning = in.readLine();
-                            dict.put(query, newMeaning);
-                            response = "0";
-                        }
-                        break;
-                    case "remove":
-                        if (dict.containsKey(query)) {
-                            dict.remove(query);
-                            response = "0";
-                        }
-                        else {
-                            response = "1";
-                        }
-                        break;
-                }
-                out.write(response + "\n");
-                out.flush();
-            }
-            catch (IOException ioe1) {
-                System.err.println("System failed to read data from socket stream.");
-                try {
-                    client.close();
-                }
-                catch (IOException ioe2) {
-                    System.err.println("System failed to close faulty client socket.");
-                }
-            }
+            // get a thread from the pool and assign the newly connected client to it
+            pool.execute(new ServerThread(client, dict));
         }
     }
 }
